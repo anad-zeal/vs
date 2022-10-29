f1a5901dfaf24008cb948c70535262263d0edd14
anad-zeal <elza.dana@gmail.com>
2022-10-29 09:23:56 -0600

    Create product-menu.php

---
+++ pages/product-menu.php
@@ -0,0 +1,94 @@
+<div class="centerflipcards clearfix">
+	<div class="flex-grid-thirds">
+		<div class="col">
+			<div class="col-md-4">
+				<div class="card-flip">
+					<div class='frontCard invCard'>
+						<div class="card-container">
+							<h2>frontCard invCard L</h2>
+						</div>
+					</div>
+					<div class='backCard invCard2'>
+						<div class='card-container2'>
+							<button class='flip-button'>SDS</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="col-md-4">
+				<div class="card-flip">
+					<div class='frontCard applyCard'>
+						<div class="card-container"> <i class="fa fa-check"></i>
+							<h2 class="textshadow">Drive Home Today!</h2>
+							<h3 class="textshadow">Bad Credit? No Credit? We have financing options for all credit needs.</h3> </div>
+					</div>
+					<div class='backCard applyCard2'>
+						<div class="card-container2"> <button class="boxshadow flip-button" href="[SSLDomain]/creditapp.aspx">Apply Today</button> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="col-md-4">
+				<div class="card-flip">
+					<div class='frontCard directionsCard'>
+						<div class="card-container"> <i class="fa fa-map-marker"></i>
+							<h2 class="textshadow">Test Drive Now</h2>
+							<h3 class="textshadow">We are conveniently located on Street Address in the City.</h3> </div>
+					</div>
+					<div class='backCard directionsCard2'>
+						<div class="card-container2"> <button class="boxshadow flip-button" href="/directions.aspx">Get Directions</button> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="flex-grid-thirds above">
+		<div class="col">
+			<div class="col-md-4">
+				<div class="card-flip">
+					<div class='frontCard invCard'>
+						<div class="card-container"> <i class="fa fa-tachometer"></i>
+							<h2 class="textshadow">See Our Vehicles</h2>
+							<h3 class="textshadow">View our online showroom. City's finest pre-owned vehicles.</h3> </div>
+					</div>
+					<div class='backCard invCard2'>
+						<div class="card-container2">
+							<button class="boxshadow flip-button" href="/newandusedcars.aspx?clearall=1">View Inventory</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="col-md-4">
+				<div class="card-flip">
+					<div class='frontCard applyCard'>
+						<div class="card-container"> <i class="fa fa-check"></i>
+							<h2 class="textshadow">Drive Home Today!</h2>
+							<h3 class="textshadow">Bad Credit? No Credit? We have financing options for all credit needs.</h3> </div>
+					</div>
+					<div class='backCard applyCard2'>
+						<div class="card-container2"> <button class="boxshadow flip-button" href="[SSLDomain]/creditapp.aspx">Apply Today</button> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="col-md-4">
+				<div class="card-flip">
+					<div class='frontCard directionsCard'>
+						<div class="card-container"> <i class="fa fa-map-marker"></i>
+							<h2 class="textshadow">Test Drive Now</h2>
+							<h3 class="textshadow">We are conveniently located on Street Address in the City.</h3> </div>
+					</div>
+					<div class='backCard directionsCard2'>
+						<div class="card-container2"> <button class="boxshadow flip-button" href="/directions.aspx">Get Directions</button> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>.
